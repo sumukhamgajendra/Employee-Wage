c0fb4479277b9a555211a50d002472c22ee5c670
yesh111 <yyaswanthpolisetti@gmail.com>
2026-01-08 16:30:29 +0530

    feat:[Sam]-added part time logic

--- src/main.py
+++ src/main.py
@@ -1,1 +1,6 @@
+from models.emp import FullTimeEmployee
+
 print("Welcome to Emp")
+
+ft1 = FullTimeEmployee("Sam",34,12000)
+print(f"{ft1.name}, {ft1.salary}")
--- src/models/emp.py
+++ src/models/emp.py
@@ -16,4 +16,13 @@ class FullTimeEmployee(Employee):
 
     def calculate_wage(self):
         return self.salary
+
+class PartTimeEmployee(Employee):
     
+    def __init__(self,name,id,hrs,no_of_days):
+        super().__init__(name, id)
+        self.no_of_days = no_of_days
+        self.hrs = hrs
+    
+    def calculate_wage(self):
+        return self.hrs * self.no_of_days
